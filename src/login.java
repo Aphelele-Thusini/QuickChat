7ee93d651977b8e29a983c0ef5ed6b464975c1de
Student <Student@LabVM2047644.st5xraxmezxu1jmnubbrxuusad.jnbx.internal.cloudapp.net>
2026-04-10 11:42:46 +0200

    I have written the login code

---
+++ src/login.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Student
+ */
+public class login {
+    //Storing the user's details after registration
+    String username;
+    String password;
+    String phonenumber;
+    String firstname;
+    String lastname;
+    
+}
