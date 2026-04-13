969f8c9172cfd5925bc233d49cee4bacbb3efd01
Student <Student@LabVM2047644.st5xraxmezxu1jmnubbrxuusad.jnbx.internal.cloudapp.net>
2026-04-13 10:46:54 +0200

    i have wriiten my booleans to check details entered

--- src/quick/chat/QuickChat.java
+++ src/quick/chat/QuickChat.java
@@ -4,11 +4,60 @@
  */
 package quick.chat;
 
+import java.util.Scanner;
 /**
  *
  * @author Student
  */
 public class QuickChat {
+    class quickChat{
+        String Storedusername;
+        String Storedpassword;
+        String storedcellphone;
+        Scanner inputuser =new Scanner (System.in);
+        
+        // boolean to check username
+        boolean Checkuserename (String username){
+            if(username.contains("_") && username.length()==5){return true;
+        }else{
+                System.out.println("username not correctly formatted"); 
+                return false;
+               
+            }
+    }
+        // boolean to check password
+        boolean checkpassword ( String password){
+            if (password.contains ("Aphelele_@18")){
+            return true;
+        }else{
+                System.out.println("Password is incorrect.");
+                return false;
+               
+            }
+        }
+        // boolean to check cellphone
+        boolean checkcellphone(string cellphone){
+            if (cellphone.length()=10 && cellphone.contains("+27")){
+            return true;
+            }else{
+                System.out.println(" cell phone number incorrectly formatted or does not contain international code");
+                return false;
+            }
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        }
 
     /**
      * @param args the command line arguments
